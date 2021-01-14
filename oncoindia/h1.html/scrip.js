e2d92dc7a04bcab70f6bc3184051acc24788da46
roshan7774 <70471441+roshan7774@users.noreply.github.com>
2021-01-14 12:24:07 +0530

    Add files via upload

---
+++ oncoindia/h1.html/scrip.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " Which crop is sown on the largest area in India?",
+    answer: "Rice",
+    options: [
+      "Rice",
+      "Wheat",
+      "Sugarcane",
+      "Maize"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Entomology is the science that studies",
+    answer: "Insects",
+    options: [
+      " Behaviour of human beings",
+      "Insects",
+      "The origin and history of technical and scientific terms",
+      "The formation of rocks"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Grand Central Terminal, Park Avenue, New York is the world's",
+    answer: "largest railway station",
+    options: [
+      "highest railway station",
+      "longest railway station",
+      "largest railway station",
+      "None of the above"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
